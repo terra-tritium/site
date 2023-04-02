84f922c403f2ac52a779151e4f90a4084bec21d5
Fábio Rodrigues <binho_mal@hotmail.com>
2023-04-02 20:23:14 -0300

    Update Website

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -12,7 +12,7 @@ function Carousel() {
         <div className="mpl-box-sm bg-light1">
             <div className="sponsors">
                 <h2 className="display-2 mb-60 text-center" data-sr data-sr-duration="1200" data-sr-distance="20">
-                Partners & Sponsors
+                    Partners & Sponsors
                 </h2>
             </div>
 
@@ -24,12 +24,11 @@ function Carousel() {
                     data-loop="true"
                     data-slides="1"
                     data-breakpoints="1200:7,992:6,767:5,575:4,440:3,320:2">
-                    <img src={Logo1} className="logo1" alt="" />
-                    <img src={Logo2} className="logo2" alt="" />
-                    <img src={Logo3} className="logo3" alt="" />
-                    <img src={Logo4} className="logo4" alt="" />
-                    <img src={Logo5} className="logo5" alt="" />
-
+                    <a href="https://www.garudanodes.com/" class="mpl-link-brand"><img src={Logo1} className="logo1" alt="" /></a>
+                    <a href="https://openai.com/blog/chatgpt" class="mpl-link-brand"><img src={Logo2} className="logo2" alt="" /></a>
+                    <a href="https://threejs.org/" class="mpl-link-brand"><img src={Logo3} className="logo3" alt="" /></a>
+                    <a href="https://www.rust-lang.org/" class="mpl-link-brand"><img src={Logo4} className="logo4" alt="" /></a>
+                    <a href="https://coinmarketcap.com/currencies/terra-luna/" class="mpl-link-brand"><img src={Logo5} className="logo5" alt="" /></a>
                 </div>
             </div>
         </div>
