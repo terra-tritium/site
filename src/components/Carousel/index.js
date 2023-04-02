b4254a576b4e6f4838bb15c9748e211c20cef813
Fábio Rodrigues <binho_mal@hotmail.com>
2023-04-02 07:46:52 -0300

    Change Swipper

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,46 +1,33 @@
-import React, { useState, useEffect } from "react";
-import SwiperCore, { Autoplay, Pagination } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper-bundle.css";
-import Logo1 from "../../assets/logo1.png";
-import Logo2 from "../../assets/logo2.png";
-import Logo3 from "../../assets/logo3.png";
-import Logo4 from "../../assets/logo5.png";
-import Logo5 from "../../assets/logo6.png";
+import React from "react";
+import Logo1 from '../../assets/logo1.png'
+import Logo2 from '../../assets/logo2.png'
+import Logo3 from '../../assets/logo3.png'
+import Logo4 from '../../assets/logo5.png'
+import Logo5 from '../../assets/logo6.png'
 import "./style.css";
 
-SwiperCore.use([Autoplay, Pagination]);
-
 function Carousel() {
-  const logos = [Logo1, Logo2, Logo3, Logo4, Logo5];
-  const [currentSlide, setCurrentSlide] = useState(0);
 
-  useEffect(() => {
-    const mySwiper = new Swiper(".swiper-container", {
-      loop: true,
-      slidesPerView: "auto",
-      centeredSlides: true,
-      spaceBetween: 80,
-      navigation: {
-        prevEl: ".swiper-button-prev",
-        nextEl: ".swiper-button-next",
-      },
-    });
-  }, []);
+    return (
+        <div className="mpl-box-sm bg-light1">
+            <div className="container">
+                <div className="mpl-carousel mpl-carousel-vertical-center text-center"
+                    data-gap="30"
+                    data-autoplay="5000"
+                    data-loop="true"
+                    data-slides="1"
+                    data-breakpoints="1200:7,992:6,767:5,575:4,440:3,320:2">
+                    <img src={Logo1} className="logo1" alt="" />
+                    <img src={Logo2} className="logo2" alt="" />
+                    <img src={Logo3} className="logo3" alt="" />
+                    <img src={Logo4} className="logo4" alt="" />
+                    <img src={Logo5} className="logo5"alt="" />
+                    
+                </div>
+            </div>
+        </div>
 
-  return (
-    <div className="mpl-carousel mpl-carousel-vertical-center text-center swiper-container">
-  <div className="swiper-wrapper">
-    {logos.map((logo, index) => (
-      <div key={index} className="swiper-slide">
-        <img src={logo} alt={`Logo ${index}`} />
-      </div>
-    ))}
-  </div>
-  <div className="swiper-button-prev"></div>
-  <div className="swiper-button-next"></div>
-</div>
-  );
+    );
 }
 
 export default Carousel;
